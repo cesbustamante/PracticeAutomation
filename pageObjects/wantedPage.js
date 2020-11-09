0d40587b4931057d00d296f206aebccc744b5083
cesar_bustamante <cesbustamante@gmail.com>
2020-11-09 15:37:57 -0600

    t

---
+++ pageObjects/wantedPage.js
@@ -0,0 +1,24 @@
+module.exports = {
+    url: 'http://localhost:3000/#/',
+    elements: {
+        //Input Fields selectors 
+        inputHeader: 'iput[name="hdrInput"]',
+        inpuMKE: 'input[name="mkeInput"]',
+        inpuOriginatingAgencyIdentifier: 'input[name="oriInput"]',
+        inpuName: 'input[name="namInput"]',
+        inpuSex: 'input[name="sexInput"]',
+        inpuRace: 'input[name="racInput"]',
+        inpuHeight: 'input[name="hgtInput"]',
+        inpuWeight: 'input[name="wgtInput"]',
+        inpuHair: 'input[name="haiInput"]',
+        inpuOffense: 'input[name="offInput"]',
+        inpuDateofWarrant: 'input[name="dowInput"]',
+        //submit button
+        burgerB: '.bm-burger-button',
+        EnterWantedB: 'p[name="enterOption"]',
+        submitB: '#saveBtn',
+        clearB: '#clearBtn'
+
+
+    }
+}
